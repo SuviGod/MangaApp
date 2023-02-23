30685f794e5615f93df38ad3fe79298525dcc803
SuviGod <sulima.ivan@lll.kpi.ua>
2023-02-23 15:29:38 +0200

    Initial commit

---
+++ src/main/java/ua/sulima/mangaapp/MangaAppApplication.java
@@ -0,0 +1,13 @@
+package ua.sulima.mangaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MangaAppApplication.class, args);
+    }
+
+}
