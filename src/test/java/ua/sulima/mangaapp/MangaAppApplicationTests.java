92404329dec1476012c39c3e3b704dd67cad708b
SuviGod <sulima.ivan@lll.kpi.ua>
2023-05-09 02:18:23 +0300

    Initial commit

---
+++ src/test/java/ua/sulima/mangaapp/MangaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.sulima.mangaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
