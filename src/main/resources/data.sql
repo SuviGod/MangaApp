95a4f36676f7a98cf3de8f1443adc2895c42eea3
SuviGod <sulima.ivan@lll.kpi.ua>
2023-07-14 02:02:33 +0300

    feat + refactor: add group table + refactor other tables + add initial group at initialization

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,13 @@
 insert into ROLES(name)
 values ('ROLE_USER'), ('ROLE_ADMIN'), ('ROLE_MODERATOR');
 
+
 insert into users(email, nickname, password)
 values ('FirstUserEmail', 'FirstUserNickname', 'FirstUserPassword');
 
-INSERT INTO creator(name, description) VALUES ('MOCHIZUKI Jun', '');
+INSERT INTO creators(name, description) VALUES ('MOCHIZUKI Jun', '');
 
-insert into manga(name, author_id, artist_id, release_year, translator_id, alternative_names, add_datetime, update_datetime, approval_datetime, preview_image_path, is_approved)
+INSERT INTO groups(group_name, link1) values ('FirstTranslationTeam', 'mylink1.com');
+
+insert into mangas(name, author_id, artist_id, release_year, group_id, alternative_names, add_datetime, update_datetime, approval_datetime, preview_image_path, is_approved)
 VALUES ('Pandora hearts', 1, 1, 1000, 1, '', now(), now(), now(), 'path', false);
